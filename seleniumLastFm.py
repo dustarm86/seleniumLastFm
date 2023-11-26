766d41da24ac946df5a3262c7aa1419eed9698fe
Dustin Armstrong <dieter@sandpawsM1MBP.local>
2023-11-25 22:43:13 -0500

    added browser click of xpath element for 1st top artist

--- seleniumLastFm.py
+++ seleniumLastFm.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 browser.get("https://www.last.fm/user/dustArm421")
 get_title = browser.title
 assert "dustArm421’s Music Profile | Last.fm" in browser.title == get_title
-#print(get_title)
+time.sleep(5)
+topArtist1 = browser.find_element("xpath", "/html/body/div[6]/div[2]/div[5]/div[3]/div/div[1]/section[3]/div/ol/li[1]/div/a").click()
 time.sleep(3)
 browser.quit()
